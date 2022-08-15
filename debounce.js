e6744bd193fd2840558d9b4f544e95cdde8cabe8
Roger Zhang <1787009521@qq.com>
2022-08-15 20:44:21 +0800

    feat: 完成防抖

---
+++ debounce.js
@@ -0,0 +1,11 @@
+function debounce(fn, time) {
+    let timer
+    return () => {
+        if(timer) {
+            clearTimeout(timer)
+        }
+        timer = setTimeout(() => {
+            fn()
+        }, time)
+    }
+}
